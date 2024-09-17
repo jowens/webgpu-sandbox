178996bd8b484abb0e68521d0e368de36256fda3
John Owens <jdowens@google.com>
2024-09-17 15:21:15 -0700

    tweakpane appears to work, thanks gman@ for the helper app

---
+++ pane-helper.js
@@ -0,0 +1,52 @@
+// by gman@google.com, 17 Sept 2024
+// bridges gap between webgpu-utils's uniform handling
+//   and tweakpane's parameter setting
+
+// Creates a factory for an object that gets/sets elements of an array
+// based on accessors (like x, y, z or r, g, b)
+function makeNamedIndexProxyMaker(indices) {
+  const keys = indices.split("");
+  return function (target) {
+    const o = {};
+    Object.defineProperties(
+      o,
+      Object.fromEntries(
+        keys.map((key, i) => {
+          return [
+            key,
+            {
+              enumerable: true,
+              get: () => target[i],
+              set: (v) => (target[i] = v),
+            },
+          ];
+        })
+      )
+    );
+    return o;
+  };
+}
+
+// Make a function that adds a binding to a pane for an array of values
+// where the values are accessed by accessor or x, y, z, or r, g, b
+// because tweakpane wants that (T_T)
+function makeAccessorHelper(indices) {
+  const make = makeNamedIndexProxyMaker(indices);
+  return function (pane, params, property, options = {}) {
+    const localParams = { value: make(params[property]) };
+    window.l = localParams;
+    return pane.addBinding(localParams, "value", {
+      label: property,
+      ...options,
+      color: { type: "float" },
+    });
+  };
+}
+
+const rgbHelper = makeAccessorHelper("rgb");
+const xyzHelper = makeAccessorHelper("xyz");
+
+// how to set one value
+//  pane.addBinding(fsUniformValues.views.mix, '0', { min: 0, max: 1, label: 'mix' });
+//  rgbHelper(pane, fsUniformValues.views, 'color');
+//  xyzHelper(pane, fsUniformValues.views, 'lightDirection');
